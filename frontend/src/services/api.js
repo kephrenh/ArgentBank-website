f42068b9a8c8a8214d18a034d63877132a72bc3a
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-18 18:58:53 +0100

    Configuration des services APi

---
+++ frontend/src/services/api.js
@@ -0,0 +1,48 @@
+import { getLoginData, getLoginFetchData, saveUserProfileData } from "./apiData.js";
+
+// Get Login
+export const getLogin = async credentials => {
+  const API_URL = "http://localhost:3001/api/v1/user/login";
+
+  const loginResponse = await fetch(API_URL, {
+    body: JSON.stringify(credentials),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  }).then(response => response.json());
+  console.clear();
+  return await getLoginData(loginResponse);
+};
+
+// Check if user is connected
+export const getLoginFetch = async token => {
+  const API_URL = "http://localhost:3001/api/v1/user/profile";
+
+  const loginFetchResponse = await fetch(API_URL, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer" + token,
+    },
+    method: "POST",
+  }).then(response => response.json());
+
+  console.clear();
+  return await getLoginFetchData(loginFetchResponse);
+};
+
+// Save new name
+export const saveUserProfile = async (token, fullName) => {
+  const API_URL = "http://localhost:3001/api/v1/user/profile";
+
+  const saveUserProfileResponse = await fetch(API_URL, {
+    body: JSON.stringify(fullName),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer" + token,
+    },
+    method: "PUT",
+  }).then(response => response.json());
+  console.clear();
+  return await saveUserProfileData(saveUserProfileResponse);
+};
---
+++ frontend/src/services/apiData.js
@@ -0,0 +1,50 @@
+/* Get Login Data */
+
+export function getLoginData(data) {
+  if (data.status !== 400) {
+    const object = {
+      status: data.status,
+      message: data.message,
+      token: data.body.token,
+    };
+    return object;
+  } else {
+    const object = {
+      status: data.status,
+      message: data.message,
+    };
+    return object;
+  }
+}
+
+/* Get Login Fetch */
+
+export function getLoginFetchData(data) {
+  if (data.body !== undefined) {
+    const object = {
+      status: data.status,
+      email: data.body.email,
+      firstName: data.body.firstName,
+      lastName: data.body.lastName,
+    };
+
+    return object;
+  } else {
+    const object = {
+      status: 0,
+      email: "",
+      firstName: "",
+      lastName: "",
+    };
+    return object;
+  }
+}
+
+/* Save User Profile Data */
+export function saveUserProfileData(data) {
+  const object = {
+    status: data.status,
+    message: data.message,
+  };
+  return object;
+}
