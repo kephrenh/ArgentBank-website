84598e5ff110d2b4e87318e4a81846b145844902
Mohamed Amoussa <126504124+kephrenh@users.noreply.github.com>
2024-02-23 19:25:56 +0100

    Add: Composant Account

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,41 +1,23 @@
 import React from "react";
+import Account from "../components/Account";
 import ProfileHeader from "../components/ProfileHeader";
+import { transactionData } from "../data/data";
 
 const User = () => {
   return (
     <main className="main bg-dark">
       <ProfileHeader />
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      {transactionData.map((transaction) => {
+        return (
+          <Account
+            key={"account" + transaction.id}
+            title={transaction.title}
+            amount={transaction.amount}
+            description={transaction.description}
+          />
+        );
+      })}
     </main>
   );
 };
