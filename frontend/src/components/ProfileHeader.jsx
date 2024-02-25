b1afc6b09f7e1d0703096041c075abe261ec34e5
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-26 00:23:11 +0100

    ProfileHeader: Display firstName and lastName, and Show Edit section

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getFirstName } from "../features/firstNameSlice";
+import { getLastName } from "../features/lastNameSlice";
+import { getLoginFetch } from "../services/api";
+import EditSection from "./EditSection/EditSection";
 
-const ProfileHeader = ({ title, firstName, lastName }) => {
+const ProfileHeader = ({ title }) => {
+  const firstName = useSelector(state => state.firstName.value);
+  const lastName = useSelector(state => state.lastName.value);
+  const token = useSelector(state => state.token.value);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const user = getLoginFetch(token);
+    user.then(object => {
+      dispatch(getFirstName(object.firstName));
+      dispatch(getLastName(object.lastName));
+    });
+  }, [token, dispatch]);
+
+  const showEditSection = e => {
+    e.preventDefault();
+    const editSection = document.querySelector(".edit-section");
+    editSection.style.display = "block";
+  };
+
   return (
     <div className="header">
       <h1>
@@ -8,7 +33,10 @@ const ProfileHeader = ({ title, firstName, lastName }) => {
         <br />
         {firstName} {lastName}!
       </h1>
-      <button className="edit-button">Edit Name</button>
+      <button className="edit-button" onClick={showEditSection}>
+        Edit Name
+      </button>
+      <EditSection />
     </div>
   );
 };
