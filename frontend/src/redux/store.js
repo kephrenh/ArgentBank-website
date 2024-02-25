c1e4099d7e601bdcd25cf2495607322022fb6190
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-26 00:16:50 +0100

    Add: userNameReducer to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import firstNameReducer from "../features/firstNameSlice.js";
 import lastNameReducer from "../features/lastNameSlice.js";
 import tokenReducer from "../features/tokenSlice.js";
+import userNameReducer from "../features/userNameSlice.js";
 
 const store = configureStore({
   reducer: {
     firstName: firstNameReducer,
     lastName: lastNameReducer,
+    userName: userNameReducer,
     token: tokenReducer,
   },
 });
