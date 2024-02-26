39c20618dee3f07ba13f23a89422cf51582cedf1
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-26 18:03:44 +0100

    Add: Error page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -30,6 +30,10 @@ const router = createBrowserRouter([
         element: <User />,
         errorElement: <Error />,
       },
+      {
+        path: "*",
+        element: <Error />,
+      },
     ],
   },
 ]);
--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <main className="error">
+      <section className="error-section">
+        <h2 className="error-title">404</h2>
+        <p className="error-message">Page not found</p>
+        <Link to="/" className="error-link">
+          Back to homepage
+        </Link>
+      </section>
+    </main>
+  );
 };
 
 export default Error;
