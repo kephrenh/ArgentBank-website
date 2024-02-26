b22ae48e18aab72ea5a68a9d5c69c89246cbca05
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-26 18:06:45 +0100

    Fix: Signin page's title element

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -6,7 +6,7 @@ const SignIn = () => {
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
-        <h1>Sign In</h1>
+        <h2>Sign In</h2>
         <LoginForm buttonText="Sign In" />
       </section>
     </main>
