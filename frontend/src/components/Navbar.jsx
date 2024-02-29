f49965d52a5de38cdcc73b6a2931b83bf80429c9
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-29 12:02:59 +0100

    Optimisation des images

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
   return (
     <nav className="main-nav">
       <NavLink className="main-nav-logo" to="/">
-        <img className="main-nav-logo-image" src="argentBankLogo.png" alt="Argent Bank Logo" />
+        <img className="main-nav-logo-image" src="argentBankLogo.webp" alt="Argent Bank Logo" />
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
 
--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,18 +1,18 @@
 export const featuresData = [
   {
-    imgSrc: "icon-chat.png",
+    imgSrc: "icon-chat.webp",
     alt: "Chat Icon",
     title: "You are our #1 priority",
     text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
   },
   {
-    imgSrc: "icon-money.png",
+    imgSrc: "icon-money.webp",
     alt: "Money Icon",
     title: "More savings means higher rates",
     text: "The more you save with us, the higher your interest rate will be!",
   },
   {
-    imgSrc: "icon-security.png",
+    imgSrc: "icon-security.webp",
     alt: "Security Icon",
     title: "Security you can trust",
     text: "We use top of the line encryption to make sure your data and money is always safe.",
